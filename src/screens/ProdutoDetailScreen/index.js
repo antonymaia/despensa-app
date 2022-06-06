414146b8857465975a36c92a65063209f34dfbc4
Antony Maia <antonymaiati@gmail.com>
2022-06-06 13:14:08 -0300

    Ajustando tela de detalhe de produto

--- src/screens/ProdutoDetailScreen/index.js
+++ src/screens/ProdutoDetailScreen/index.js
@@ -1,19 +1,26 @@
 import DateTimePicker from "@react-native-community/datetimepicker";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, TextInput, StyleSheet } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 import { Button } from "react-native-paper";
 
 export const ProdutoDetailScreen = ({ produto, setShowModal }) => {
   const [getProduto, setProduto] = useState(produto);
-  const [date, setDate] = useState(new Date());
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    fetch("https://despensaapi.herokuapp.com/categoria", { method: "GET" })
+      .then((res) => res.json())
+      .then((json) => setCategorias(json));
+  }, []);
 
   const setDataValidade = (event, date) => {
     setProduto({ ...getProduto, dataValidade: date.toLocaleDateString("pt-BR") });
   };
   return (
     <View style={styles.container}>
-      <View style={{marginVertical: 20}}>
+      <View style={{ marginVertical: 20 }}>
         <Text style={styles.label}>Nome:</Text>
         <TextInput
           style={styles.input}
@@ -34,10 +41,21 @@ export const ProdutoDetailScreen = ({ produto, setShowModal }) => {
         />
       </View>
       <View>
-      <Text style={styles.label}>Categoria:</Text>
+        <Text style={styles.label}>Categoria:</Text>
+        <View>
+          <Picker mode="dropdown"  selectedValue="0" dropdownIconColor={'white'}>
+            <Picker.Item color="white" value={0} label="Selecione uma categoria"/>
+            {categorias.map((ct, index) => {
+              return (
+                <Picker.Item color="white" key={index} label={ct.nome} value={ct.id} />
+              );
+            })}
+          </Picker>
+        </View>
       </View>
       <Button
         onPress={() => {
+          console.log(getProduto);
           setShowModal(false);
         }}
       >
@@ -55,13 +73,13 @@ const styles = StyleSheet.create({
   },
   label: {
     color: "white",
-    fontSize: 24,
+    fontSize: 22,
     paddingVertical: 10,
   },
   input: {
     padding: 10,
     backgroundColor: "#362C5A",
-    fontSize: 20,
+    fontSize: 18,
     color: "white",
     borderRadius: 10,
   },
